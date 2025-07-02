03970eb492648c3fd94db729246a118a37e48591
wosohd <wosohd@gmail.com>
2025-07-02 05:58:18 +0300

    More styling, restructuring and debugging

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from datetime import datetime
 
 # Initialize extensions
 db = SQLAlchemy()
@@ -31,6 +32,10 @@ def create_app():
     app.register_blueprint(main_blueprint)
     app.register_blueprint(sales_blueprint)
     app.register_blueprint(maintenance_blueprint)
+
+    @app.context_processor
+    def inject_current_year():
+        return {'current_year': datetime.now().year}
 
     return app
 
